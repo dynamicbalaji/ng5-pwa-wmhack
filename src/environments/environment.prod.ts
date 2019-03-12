14642ad286c551d7465d3afcca9892e959e13735
dynamicbalaji <dynamicbalaji4u@gmail.com>
2019-03-12 21:46:44 +0530

    Removing service details from environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   dialogflow: {
     angularBot: 'e3a17f2629de4f15a300370e5e332266'
   },
-  apiEndPoint: 'http://10.177.138.36:8080/ServicePoc/',
-  dialogFlowService:'http://10.177.138.37:3000/policy/',
-  chatApiEndPoint : 'http://10.177.138.37:3000/policy/'
+  apiEndPoint: 'http://localhost:8080/ServicePoc/',
+  dialogFlowService:'http://localhost:3000/policy/',
+  chatApiEndPoint : 'http://localhost:3000/policy/'
 };
